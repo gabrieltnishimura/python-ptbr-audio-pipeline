ec49bb4f2890d938ca94ace5cf0652e7dab73c76
Gabriel Nishimura <gabrieltnishimura@gmail.com>
2019-06-02 11:23:32 -0300

    feat(specgram): downloads and saves spectogram fft=254 and overlap=127

--- start.py
+++ start.py
@@ -1,16 +1,25 @@
 import urllib.request
 import urllib.parse
+import os
 from pydub import AudioSegment
 
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.io import wavfile
+
 corpus_path = 'corpus.txt'
-MP3_EXTENSION = '.mp3'
+FOLDER = './assets/'
+LABEL_FOLDER = './labels/'
+LABEL_EXTENSION = 'png'
+EXPORT_TO_TYPE = 'wav'
+FILE_EXTENSION = '.mp3'
 
 # Download and open audio in-memory
 def processAudio(word):
     host = 'https://www.ispeech.org/p/generic/getaudio?action=convert&pitch=100&voice=brportuguesefemale&speed=0&'
     query = urllib.parse.urlencode({'text': word})
-    urllib.request.urlretrieve(host + query, word + MP3_EXTENSION)
-    song = AudioSegment.from_mp3(word + MP3_EXTENSION)
+    urllib.request.urlretrieve(host + query, FOLDER + word + FILE_EXTENSION)
+    song = AudioSegment.from_mp3(FOLDER + word + FILE_EXTENSION)
     return song
 
 # Crop audio and return
@@ -20,12 +29,25 @@ def cropAudio(song):
     extract = song[0:endTime]
     return extract
 
+def generateSpectogram(word):
+    sample_rate, samples = wavfile.read(FOLDER + word + '.' + EXPORT_TO_TYPE)
+    frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate, nperseg=254, nfft=254, noverlap=127)
+    plt.pcolormesh(times, frequencies, spectrogram)
+    plt.axis('off')
+    plt.savefig(LABEL_FOLDER + word + '.' + LABEL_EXTENSION, bbox_inches='tight', pad_inches = 0)
+
 # Open Corpus
 with open(corpus_path) as fp:  
     line = fp.readline()
     while line:
+        print('processing ' + line)
         word = line.strip()
         song = processAudio(word)
         extract = cropAudio(song)
-        extract.export(word + MP3_EXTENSION, format='mp3')
+        extract.export(FOLDER + word + '.' + EXPORT_TO_TYPE, format=EXPORT_TO_TYPE)
+        os.remove(FOLDER + word + FILE_EXTENSION)
+        generateSpectogram(word)
         line = fp.readline()
+
+
+
