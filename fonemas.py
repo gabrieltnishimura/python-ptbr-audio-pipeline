f7441431f74c5e1e8a9a00b73f80f18a484c8369
Gabriel Nishimura <gabrieltnishimura@gmail.com>
2019-06-18 03:28:51 -0300

    feat(phonemes): restart words array

--- fonemas.py
+++ fonemas.py
@@ -1,20 +1,42 @@
+from time import sleep
 from pyquery import PyQuery
 import requests
 import re
 import math
 import numpy as np
 
-WORDS_FILE = '_words.npy'
-PHONEMES_FILE = '_phonemes.npy'
+WORDS_FILE = '-words.npy'
+PHONEMES_FILE = '-phonemes.npy'
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-           'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+           'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x' 'y', 'z']
 words, phonemes = [], []
+
+
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 *
+                                                     (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end='\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
 
 
 def get_html(letter: str, page: str):
     url = 'http://www.portaldalinguaportuguesa.org/index.php?action=fonetica&region=spx&act=list&letter=%s&start=%s' % (
         letter, int(page)*20)
-    print('Letter: \'' + str(letter) + '\', page ' + str(page))
     return requests.get(url).text
 
 
@@ -32,7 +54,6 @@ def get_result_pages(html):
 def loop_letter(letter):
     response = get_html(letter, 0)
     total_pages = get_result_pages(response)
-    print('Total pages for letter: \'' + str(letter) + ', ' + str(total_pages))
     for i in range(0, total_pages):
         response = get_html(letter, i)
         pq = PyQuery(response)
@@ -42,20 +63,26 @@ def loop_letter(letter):
             if word != '' and phoneme != '' and word not in words:
                 words.append(word)
                 phonemes.append(phoneme)
-
-
-def process():
-    for letter in letters:
-        loop_letter(letter)
-        persist(letter)
-        break
+        printProgressBar(i, total_pages, prefix='Progress:',
+                         suffix='Complete (' + str(total_pages) + ')', length=50)
 
 
 def persist(letter: str):
+    print('Saving ' + str(len(words)) + ' words on letter \'' + letter + '\'')
     npwords = np.array(words)
     npphonemes = np.array(phonemes)
     np.save(letter+WORDS_FILE, npwords)
     np.save(letter+PHONEMES_FILE, npphonemes)
+    words.clear()
+    phonemes.clear()
+
+
+def process():
+    for letter in letters:
+        print('Letter: \'' + str(letter) + '\'')
+        loop_letter(letter)
+        persist(letter)
+        print('Finished letter: \'' + str(letter) + '\'')
 
 
 process()
